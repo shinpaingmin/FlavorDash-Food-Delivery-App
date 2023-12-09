77598fcb21d98bb096673f0549dbfa62d085ebaa
shinpaingmin <shinpaingmin@gmail.com>
2023-12-09 22:58:25 +0630

    added images

---
+++ react/src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero1 from './hero-1.webp';
+import mainPic from './main-pic.jpg';
+import pizza from './pizza.jpg';
+import burger from './burger.jpg';
+import dumpling from './dumpling.jpg';
+import noodle from './noodle.jpg';
+
+export {
+    hero1,
+    mainPic,
+    pizza,
+    burger,
+    dumpling,
+    noodle,
+}
