b816e0fc8be8bdda3e5cd1ce7f8ae1d9ecb07ba9
shinpaingmin <shinpaingmin@gmail.com>
2023-12-10 23:18:01 +0630

    added a new service card

---
+++ react/src/components/HomePage/ServiceCard.jsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+
+const ServiceCard = ({ Icon, size, title }) => {
+  return (
+    <motion.div className="grid place-items-center
+                    border border-transparent
+                    hover:border-gray-200 hover:shadow-sm cursor-pointer pt-5"
+            whileHover={{ y: -10 }}
+                  >
+
+        <div className="w-max rounded-full p-5 bg-orange
+                        ">
+            <Icon size={size} className='text-white' />
+        </div>
+
+        <div className="text-center p-6">
+            <h1 className="text-lg font-semibold mb-3">{ title }</h1>
+            <p className="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, molestiae.</p>
+        </div>
+    </motion.div>
+  )
+}
+
+export default ServiceCard
