a03a27acd5b3785493dbdf5b51ce61803b2256e1
shinpaingmin <shinpaingmin@gmail.com>
2023-12-11 23:29:09 +0630

    modified map

--- react/src/components/HomePage/Location.jsx
+++ react/src/components/HomePage/Location.jsx
@@ -2,22 +2,44 @@ import {
     APIProvider,
     Map,
     AdvancedMarker,
-    Pin
+    Pin,
+    InfoWindow
 } from "@vis.gl/react-google-maps";
+import { useState } from "react";
+import { locations } from "../../constants/mapData";
 
 
 const Location = () => {
+
+    const [open, setOpen] = useState(null);
+
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
         <div className="px-8 h-96 w-full">
             <Map zoom={12} center={{ lat: 16.8508, lng: 96.1156 }}
                     mapId={import.meta.env.VITE_GOOGLE_MAP_ID}>
-                <AdvancedMarker position={{ lat: 16.8508, lng: 96.1156 }} >
-                    <Pin background={"#ff6b35"} glyphColor={"white"}></Pin>
-                </AdvancedMarker>
-                <AdvancedMarker position={{ lat: 16.9042, lng: 96.0997 }} >
-                    <Pin background={"#ff6b35"} glyphColor={"white"}></Pin>
-                </AdvancedMarker>
+
+                {
+                    locations.map((loc) => (
+                        <div key={loc.id}>
+                        <AdvancedMarker
+                                position={{ lat: loc.lat, lng: loc.lng }}
+                                onClick={() => setOpen(loc.id)} >
+
+                            <Pin background={"#ff6b35"} glyphColor={"white"}></Pin>
+
+                        </AdvancedMarker>
+
+                        {
+                            open === loc.id && (
+                                <InfoWindow position={{ lat: loc.lat, lng: loc.lng }} onCloseClick={() => setOpen(null)}>
+                                    <p>{ loc.location }</p>
+                                </InfoWindow>
+                            )
+                        }
+                        </div>
+                    ))
+                }
             </Map>
         </div>
     </APIProvider>
