eab9567015e9d582382771ee1817ab85ade03237
shinpaingmin <shinpaingmin@gmail.com>
2023-12-11 23:27:03 +0630

    added map data

---
+++ react/src/constants/mapData.js
@@ -0,0 +1,36 @@
+export const locations = [
+    {
+        id: 1,
+        location: "Hlaing Township",
+        lat: 16.8508,
+        lng: 96.1156
+    },
+
+    {
+        id: 2,
+        location: "Insein Township",
+        lat: 16.9042,
+        lng: 96.0997
+    },
+
+    {
+        id: 3,
+        location: "Mayangon Township",
+        lat: 16.868876,
+        lng: 96.150604
+    },
+
+    {
+        id: 4,
+        location: "Kamayut Township",
+        lat: 16.8238,
+        lng: 96.1341
+    },
+
+    {
+        id: 5,
+        location: "Sanchaung Township",
+        lat: 16.8062,
+        lng: 96.1341
+    },
+]
