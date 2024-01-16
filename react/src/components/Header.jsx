8c72a8839e612275680c294d22c131a0b769f539
shinpaingmin <shinpaingmin@gmail.com>
2024-01-16 18:43:59 +0630

    second commit for shopping cart

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -4,7 +4,8 @@ import { FaTruckFast, FaLocationDot  } from "react-icons/fa6";
 import { IoCartOutline } from "react-icons/io5";
 import Button from "./Button";
 import HamburgerMenu from './HamburgerMenu';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import ShoppingCart from './ShoppingCart';
 
 const Header = ({ open, setOpen }) => {
 
@@ -118,64 +119,7 @@ const Header = ({ open, setOpen }) => {
         </div>
 
         {
-            openCart && <div className='fixed top-0 left-0 w-full h-full z-10
-            bg-black/50 flex items-center justify-center'>
-           <div className='animate w-[500px] h-[450px] bg-[#fefefe] relative overflow-auto hideScrollbar px-6 py-4'>
-
-                <h1 className='font-bold text-2xl '>Cart</h1>
-                <span className='font-bold text-3xl cursor-pointer absolute top-2.5 right-5'
-                    onClick={handleOpenCart}>&times;</span>
-
-                <div className='border-t border-t-gray-300 border-dotted mb-3'>
-                    <div className='mt-3 flex border shadow-sm'>
-                        <div className='w-32 h-24'>
-                            <img src="https://geekrobocook.com/wp-content/uploads/2021/04/KFC-Chicken-fry.jpg" alt="" className='w-full h-full object-cover bg-gray-500' />
-                        </div>
-                        <div className='p-2.5'>
-                            <h3 className='text-sm font-bold mb-3'>KFC chicken</h3>
-                            <div className='flex items-center'>
-                                <button className='rounded-full p-1 w-6 h-6 border flex items-center justify-center'>+</button>
-                                <p className='mx-3'>12</p>
-                                <button className='rounded-full p-1 w-6 h-6 border flex items-center justify-center'>-</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='mt-3 flex border shadow-sm'>
-                        <div className='w-32 h-24'>
-                            <img src="https://cicili.tv/wp-content/uploads/2020/09/Ma-la-xiang-guo-3-scaled.jpg" alt="" className='w-full h-full object-cover bg-gray-500' />
-                        </div>
-                        <div className='p-2.5'>
-                            <h3 className='text-sm font-bold'>Mala xiang gou</h3>
-                        </div>
-                    </div>
-                    <div className='mt-3 flex border shadow-sm'>
-                        <div className='w-32 h-24'>
-                            <img src="https://en.ardeche-guide.com/sites/default/files/sit/data/media/images/134244/boulieu_pizz-7728144.jpg" alt="" className='w-full h-full object-cover bg-gray-500' />
-                        </div>
-                        <div className='p-2.5'>
-                            <h3 className='text-sm font-bold'>Pizza</h3>
-                        </div>
-                    </div>
-                    <div className='mt-3 flex border shadow-sm'>
-                        <div className='w-32 h-24'>
-                            <img src="https://sudachirecipes.com/wp-content/uploads/2023/07/kimchi-nabe-thumb-500x500.jpg" alt="" className='w-full h-full object-cover bg-gray-500' />
-                        </div>
-                        <div className='p-2.5'>
-                            <h3 className='text-sm font-bold'>Hotpot</h3>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='border-t border-t-gray-300 border-dashed'>
-                    <div className='mt-3 flex justify-between items-center'>
-                        <h2 className='font-bold'>Total: $400</h2>
-                        <button className='px-4 py-2 bg-orange text-white hover:opacity-90'>
-                            Checkout
-                        </button>
-                    </div>
-                </div>
-           </div>
-       </div>
+            openCart && <ShoppingCart handleOpenCart={handleOpenCart} />
         }
         </>
     )
