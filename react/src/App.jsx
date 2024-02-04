d6fdd82a7b23448ff7b441f0bc12185f75c15a82
shinpaingmin <shinpaingmin@gmail.com>
2024-02-04 21:16:51 +0630

    added redux config and mui theme config

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,16 +1,32 @@
+import { BrowserRouter } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import NotFound from './pages/NotFound';
-import FeedPage from './pages/FeedPage';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import MenuPage from "./pages/MenuPage";
+import HomePage from './pages/customer/HomePage';
+import NotFound from './pages/customer/NotFound';
+import FeedPage from './pages/customer/FeedPage';
+import LoginPage from './pages/customer/LoginPage';
+import SignUpPage from './pages/customer/SignUpPage';
+import MenuPage from "./pages/customer/MenuPage";
+
+import AdminHomePage from './pages/restaurant/HomePage';
+
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
 
 
 function App() {
+
+    const mode = useSelector((state) => state.global.mode);
+    console.log(mode)
+
+    const theme = useMemo(() => {
+        return createTheme(themeSettings(mode))
+    }, [mode]);
+    console.log(theme)
 
     const [open, setOpen] = useState(false);
 
@@ -21,16 +37,22 @@ function App() {
 
   return (
     <>
-    <Header open={open} setOpen={setOpen} />
-        <Routes>
-            <Route path="/" element={ <HomePage /> } />
-            <Route path="feed" element={ <FeedPage /> } />
-            <Route path="login" element={ <LoginPage /> } />
-            <Route path="signup" element={ <SignUpPage /> } />
-            <Route path="menu" element={ <MenuPage /> } />
-            <Route path="*" element={ <NotFound /> } />
-        </Routes>
-    <Footer />
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+            <Header open={open} setOpen={setOpen} />
+                <Routes>
+                    <Route path="/" element={ <HomePage /> } />
+                    <Route path="feed" element={ <FeedPage /> } />
+                    <Route path="login" element={ <LoginPage /> } />
+                    <Route path="signup" element={ <SignUpPage /> } />
+                    <Route path="menu" element={ <MenuPage /> } />
+
+                    <Route path="/admin/home" element={ <AdminHomePage /> } />
+                    <Route path="*" element={ <NotFound /> } />
+                </Routes>
+            <Footer />
+            </ThemeProvider>
+        </BrowserRouter>
     </>
   )
 }
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,12 +1,19 @@
-import { BrowserRouter } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { configureStore } from '@reduxjs/toolkit';
+import globalReducer from './state';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+    reducer: {
+        global: globalReducer,
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <BrowserRouter>
+    <Provider store={store}>
         <App />
-    </BrowserRouter>
-
+    </Provider>
 )
